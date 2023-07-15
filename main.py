f6dc7678d124b0672cee08171b8f5ef4a7ea2250
Rashid Wassan <rashidwassaan@gmail.com>
2023-07-15 19:18:30 +0500

    add basics of YAML with comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import yaml
 
+# this to check the yaml file contents.
 with open('main.yaml', 'r') as file:
+    # you will se how the data is converted in json type
     print(yaml.safe_load(file))
